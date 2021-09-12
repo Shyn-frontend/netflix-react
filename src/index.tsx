192d078a4296d1e75876b248d5ae4387f0538aa0
naV-meiK <87219641+naV-meiK@users.noreply.github.com>
2021-09-12 11:34:48 +0700

    remove file unnecessary

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// eslint-disable-next-line import/named
-import { Provider } from 'react-redux';
 import App from './App';
-import { store } from './app/store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
